4635a7f338f61d107cdcbdb8cf2019be4ed99a5f
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-04-17 17:41:12 +0500

    Finished version

--- app/src/main/java/com/shevy/applicationfortask/MainActivity.kt
+++ app/src/main/java/com/shevy/applicationfortask/MainActivity.kt
@@ -7,7 +7,6 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,27 +21,20 @@ class MainActivity : AppCompatActivity() {
 
     private fun saveCount(count: Int) {
         val sharedPreference = applicationContext.getSharedPreferences("PREFERENCE_NAME", Context.MODE_PRIVATE)
-        Log.d("testLog", "sharedPreference = $sharedPreference")
         var editor = sharedPreference.edit()
-        Log.d("testLog", "editor = $editor")
         editor.putInt("Count", count)
-        Log.d("testLog", "editor = $editor")
         editor.apply()
-        Log.d("testLog", "editor = $editor")
     }
 
     @JvmName("getCount1")
     fun getCount(): Int {
         val sharedPreference = applicationContext.getSharedPreferences("PREFERENCE_NAME", Context.MODE_PRIVATE)
-        Log.d("testLog", "sharedPreference getCount() = $sharedPreference")
         count = sharedPreference.getInt("Count", 0)
-        Log.d("testLog", "getCount count = $count")
         return count
     }
 
     override fun onDestroy() {
         count++
-        Log.d("testLog", "onDestroy count = $count")
         saveCount(count)
         Log.d("testLog", "onDestroy count = $count")
         super.onDestroy()
